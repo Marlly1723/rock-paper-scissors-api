a8e5e80c8aa4117f8f013515581048d2580386fa
Marlly1723 <66263982+Marlly1723@users.noreply.github.com>
2025-11-21 16:34:28 -0500

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,19 @@
+import requests
+
+# URL de la API
+url = "http://127.0.0.1:8000/predict"
+
+# Imagen de prueba
+file_path = "test_image.jpg"
+
+# Abrir el archivo y enviar POST
+with open(file_path, "rb") as f:
+    files = {"file": (file_path, f, "image/jpeg")}
+    response = requests.post(url, files=files)
+
+# Mostrar resultado
+print("Status code:", response.status_code)
+print("Response JSON:", response.json())
 from fastapi.testclient import TestClient
 from src.app.main import app
 import os
